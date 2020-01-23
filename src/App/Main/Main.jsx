be89a9b90c6934b078d4c3ec1b9a067abe56f10c
Mike24 <59624793+hayk24@users.noreply.github.com>
2020-01-22 21:40:28 -0800

    000

--- src/App/Main/Main.jsx
+++ src/App/Main/Main.jsx
@@ -3,7 +3,17 @@ import './Main.scss';
 
 const Main = () => {
     return (
-        <main className='Main'>Componrnt Main</main>
+        <main className='Main'>
+
+         <div className="colums">
+
+            <div className='col col1'>Col1</div>
+            <div className='col col2'>Col2</div>
+            <div className='col col3'>Col3</div>
+            <div className='col col4'>Col4</div>
+             
+         </div>
+        </main>
 
     )
 };
--- src/App/Nav/Nav.jsx
+++ src/App/Nav/Nav.jsx
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Nav.scss';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
 const Nav = () => {
+
+    const [showMenu, updateShowMenu] = useState(true);
+
+    const handleHamburgerClick = () => {
+        console.log('you hit the hamburger')
+
+        let timbuktu;
+
+        if ( showMenu === true) {
+            timbuktu = false;
+
+        }else {
+            timbuktu = true;
+        }
+
+        updateShowMenu(timbuktu);
+
+    }
     return (
         <nav className='Nav'>
         
-            <div className="hamburger">Hamburger</div>
-
+            <div className="hamburger" onClick={ handleHamburgerClick }>
+                <FontAwesomeIcon icon={ faBars }  />
+            </div>
+            {
+            showMenu &&
             <div className="link">
                 <a href="#">Welcome</a>
                 <a href="#">Services</a>
                 <a href="#">Contact</a>
             </div>
-        
+            }
         </nav>
-
     )
 };
 
